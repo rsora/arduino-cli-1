f41ebf1667ffd606cff5ed340e03c4f32c3daa9f
Cristian Maglie <c.maglie@arduino.cc>
2020-04-01 16:52:00 +0200

    Infrastructure for new boards commands

--- cli/board/board.go
+++ cli/board/board.go
@@ -37,6 +37,8 @@ func NewCommand() *cobra.Command {
 	boardCommand.AddCommand(detailsCommand)
 	boardCommand.AddCommand(initListCommand())
 	boardCommand.AddCommand(listAllCommand)
-
+	boardCommand.AddCommand(initInstallCommand())
+	boardCommand.AddCommand(initUninstallCommand())
+	boardCommand.AddCommand(initSearchCommand())
 	return boardCommand
 }
